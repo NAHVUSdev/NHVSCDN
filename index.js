6b9eda2bb5cb1cff08438ad534c9af8148d956a3
NAHVUSdev <trentonenter@outlook.com>
2019-11-25 11:14:01 -0600

    fixes

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ app.get('/', function (req, res) {
 // RESOURCES
 app.get('/assets/global.css', function (req, res) {
     "use strict";
-    res.sendFile('global.css', {root: path.join(__dirname, 'app/assets')});
+    res.sendFile('global.css', {root: path.join(__dirname, 'app')});
 });
 app.get('/assets/style.css', function (req, res) {
     "use strict";
-    res.sendFile('style.css', {root: path.join(__dirname, 'app/assets')});
+    res.sendFile('style.css', {root: path.join(__dirname, 'app')});
 });
 app.get('/assets/default.js', function (req, res) {
     "use strict";
-    res.sendFile('default.js', {root: path.join(__dirname, 'app/assets')});
+    res.sendFile('default.js', {root: path.join(__dirname, 'app')});
 });
 app.get('/assets/script.js', function (req, res) {
     "use strict";
-    res.sendFile('script.js', {root: path.join(__dirname, 'app/assets')});
+    res.sendFile('script.js', {root: path.join(__dirname, 'app')});
 });
 
 app.listen(8080);
