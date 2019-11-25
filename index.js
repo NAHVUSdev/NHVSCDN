2da22dbf12083820f63dc85f96a2cc751ae6d597
NAHVUSdev <trentonenter@outlook.com>
2019-11-25 11:36:22 -0600

    fixed index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var require;
 var express = require('express');
 var app = express();
 var path = require('path');
+app.use(express.static('app'));
 
 app.get('/', function (req, res) {
     "use strict";
@@ -9,21 +10,17 @@ app.get('/', function (req, res) {
 });
 
 // RESOURCES
-app.get('/assets/global.css', function (req, res) {
+app.get('/global.css', function (req, res) {
     "use strict";
-    res.sendFile('global.css', {root: path.join(__dirname, 'app/')});
+    res.sendFile(path.join(__dirname + '/app/assets/global.css'));
 });
-app.get('/assets/style.css', function (req, res) {
+app.get('/style.css', function (req, res) {
     "use strict";
-    res.sendFile('style.css', {root: path.join(__dirname, 'app/')});
+    res.sendFile(path.join(__dirname + '/app/assets/style.css'));
 });
-app.get('/assets/default.js', function (req, res) {
+app.get('/default.js', function (req, res) {
     "use strict";
-    res.sendFile('default.js', {root: path.join(__dirname, 'app/')});
-});
-app.get('/assets/script.js', function (req, res) {
-    "use strict";
-    res.sendFile('script.js', {root: path.join(__dirname, 'app/')});
+    res.sendFile(path.join(__dirname + '/app/assets/default.js'));
 });
 
 app.listen(8080);
